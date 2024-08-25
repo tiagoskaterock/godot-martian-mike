576cb4219cca132a168efcf72184aa7a29c3accf
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-25 03:03:01 -0300

    Saw Animated even better

--- scripts/saw.gd
+++ scripts/saw.gd
@@ -1,7 +1,10 @@
-extends Area2D
+extends Node
 
 
+func _ready():
+	$Area2D/AnimatedSprite2D.play_backwards()
 
-func _on_body_entered(body):
+
+func _on_area_2d_body_entered(body):
 	if body is Player:
 		body.die()
