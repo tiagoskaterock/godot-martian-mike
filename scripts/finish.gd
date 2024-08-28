17613e87cdc52f18557f32eb2241edd3639404a2
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-28 18:47:26 -0300

    End Area Updated

--- scripts/finish.gd
+++ scripts/finish.gd
@@ -3,5 +3,10 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player:
-		body.happy()
+		$Happy.play()
 		get_parent().stop_song()
+		$AnimatedSprite2D.play("end")
+
+
+func _on_happy_finished():
+	$AnimatedSprite2D.play("idle")
