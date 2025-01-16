d8c1914a31a39fb56001ecfd6dcfcf509ec803cd
Tiago Bordin <tiagolemespalhano@gmail.com>
2025-01-16 14:43:21 -0300

    Adicionada mais camadas parallax nas montanhas e nuvens

--- scripts/level.gd
+++ scripts/level.gd
@@ -10,7 +10,8 @@ func _process(delta):
 	if Input.is_action_just_pressed("start"):
 		print('Reload')
 		get_tree().reload_current_scene()
-		
+	
+	$ParallaxBackground/BackCloudsLayer/Sprite2D.position.x -= 10 * delta
 		
 func stop_song() -> void:
 	$BGSong.stop()
