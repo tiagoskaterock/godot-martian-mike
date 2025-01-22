97073aedaf712736c4fea76c33195e78d3027363
Tiago Bordin <tiagolemespalhano@gmail.com>
2025-01-22 10:55:37 -0300

    Animação de morte nos goombas

--- scripts/goomba.gd
+++ scripts/goomba.gd
@@ -8,12 +8,24 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @export var direction : int = -1
+var death_animation : bool = false
 
 
 func _physics_process(delta):
-	handle_gravity(delta)
-	handle_horizontal_movement(delta)
+	if not death_animation:
+		handle_gravity(delta)
+		handle_horizontal_movement(delta)
+	if death_animation:
+		$CollisionShape2D.disabled = true
+		$Area2DAttack/CollisionShape2D.disabled = true
+		$Area2DStomp/CollisionShape2D.disabled = true
+		position.y -= 500 * delta
+		position.x += 500 * delta
+		rotate(30 * delta)
+		
 	move_and_slide()
+	
+	print(velocity.y)
 
 
 func handle_gravity(delta):
@@ -38,5 +50,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func die():
 	var scream = preload("res://scenes/goomba_scream.tscn").instantiate()
 	get_parent().add_child(scream)
-	queue_free()
+	death_animation = true
 	
